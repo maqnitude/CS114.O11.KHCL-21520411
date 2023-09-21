46c5f634417ab01965ac8ec4f7b972fef3e7cc84
Mai Anh Quân <108073174+qvan7um@users.noreply.github.com>
2023-09-21 20:44:00 +0700

    solved tricked problem about diagonal matrix

--- python-practice-problems/week-2/MaTranDuongCheo.py
+++ python-practice-problems/week-2/MaTranDuongCheo.py
@@ -1,13 +1,36 @@
 n = int(input())
 mat = [list(map(int, input().split())) for _ in range(n)]
 
-def is_diagonal_matrix(matrix):
+# Only 1 of the 2 diagonals is allowed to have all 1s
+# Check the primary diagonal first, then the secondary one
+# Lastly check the remaining elements
+def check(matrix):
+    valid_primary_diagonal = True
+
+    # check primary diagonal
     for i in range(n):
-        for j in range(n):
-            if i == j and matrix[i][j] != 1:
+        if matrix[i][i] != 1:
+            valid_primary_diagonal = False
+            break
+
+    if not valid_primary_diagonal: # check secondary diagonal
+        for i in range(n):
+            j = n - 1 - i
+            if matrix[i][j] != 1:
                 return 'NO'
-            if i != j and matrix[i][j] != 0:
-                return 'NO'
+    
+    if valid_primary_diagonal:
+        for i in range(n):
+            for j in range(n):
+                if i != j and matrix[i][j] != 0:
+                    return 'NO'
+    else:
+        for i in range(n):
+            k = n - 1 - i
+            for j in range(n):
+                if j != k and matrix[i][j] != 0:
+                    return 'NO'
+    
     return 'YES'
 
-print(is_diagonal_matrix(mat))
+print(check(mat))
