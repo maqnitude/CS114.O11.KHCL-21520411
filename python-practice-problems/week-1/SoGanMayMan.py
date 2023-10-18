5d4b625a4b456d52c93070b029979901f8e9459c
Mai Anh Quân <108073174+qvan7um@users.noreply.github.com>
2023-10-18 14:00:12 +0700

    remove solutions to practice problems

--- python-practice-problems/week-1/SoGanMayMan.py
+++
@@ -1,8 +0,0 @@
-number = input()
-
-lucky_count = number.count('4') + number.count('7')
-     
-if lucky_count == 4 or lucky_count == 7:
-    print("YES")
-else:
-    print("NO")
--- python-practice-problems/week-3/k_closest_numbers.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-arr = list(map(int, input().split()))
-k, x = map(int, input().split())
-
-def get_distance(number):
-    return abs(number - x)
-
-print(" ".join(map(str, sorted(sorted(arr, key=get_distance)[:k]))))
